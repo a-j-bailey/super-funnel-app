5ebbb101945d55ab0f3078c929be0c259ee95853
Adam Bailey <ajbailey564@gmail.com>
2025-07-09 22:27:59 -0400

    FAB

---
+++ components/CreateFAB.tsx
@@ -0,0 +1,48 @@
+import { ThemedText } from '@/components/ThemedText';
+import * as Haptics from 'expo-haptics';
+import { router } from 'expo-router';
+import { GestureResponderEvent, StyleSheet, TouchableOpacity, View } from 'react-native';
+
+export function CreateFAB() {
+    const navigate = (ev: GestureResponderEvent) => {
+        if (process.env.EXPO_OS === 'ios') {
+            // Add a soft haptic feedback when pressing down on the tabs.
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
+        }
+
+        router.navigate('/create')
+    }
+    
+    return (
+        <TouchableOpacity
+            onPressIn={navigate}
+        >
+            <View style={styles.container}>
+                <ThemedText style={styles.text}>+</ThemedText>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 28,
+        lineHeight: 28,
+        color: 'white',
+    },
+    container: {
+        position: 'absolute',
+        bottom: 24,
+        right: 16,
+        width: 48,
+        height: 48,
+        padding: 8,
+        backgroundColor: 'gray',
+        gap: 8,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+});
