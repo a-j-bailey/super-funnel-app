87f1431a99d394febc19700b535afb36a5463a00
Adam Bailey <ajbailey564@gmail.com>
2025-08-14 09:52:52 -0400

    update types

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,9 @@
 export type Task = {
     id: number;
     title: string;
+    priority: number;
+    description?: string;
     parentTask?: Task
     dueDate?: Date;
+    completedDate?: Date;
 };
