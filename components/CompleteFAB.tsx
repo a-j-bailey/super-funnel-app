5b2a57681bf01cd44b5fc7a030a58fa8c7087e2a
Adam Bailey <ajbailey564@gmail.com>
2025-08-21 00:35:02 -0400

    update components

--- components/CompleteFAB.tsx
+++ components/CompleteFAB.tsx
@@ -1,30 +1,28 @@
 import { useThemeColor } from '@/hooks/useThemeColor';
+import { useTasks } from '@/providers/TaskProvider';
 import * as Haptics from 'expo-haptics';
-import { router } from 'expo-router';
 import { CircleCheck } from 'lucide-react-native';
 import { GestureResponderEvent, StyleSheet, TouchableOpacity } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ThemedText } from './ThemedText';
 
-export function CompleteFAB() {
+export function CompleteFAB({ taskId }: { taskId: number }) {
+    const { markTaskCompleted } = useTasks()
     const insets = useSafeAreaInsets();
-    const bgColor = useThemeColor({}, 'tint')
     const iconColor = useThemeColor({}, 'background')
     const green = useThemeColor({ light: '#879A39', dark: '#66800B' }, 'tint')
-    // const green2 = useThemeColor({ light: '#66800B', dark: '#66800B' }, 'tint')
 
-    const navigate = (ev: GestureResponderEvent) => {
+    const complete = (ev: GestureResponderEvent) => {
         if (process.env.EXPO_OS === 'ios') {
             // Add a soft haptic feedback when pressing down on the tabs.
             Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Soft);
         }
-
-        router.navigate('/create')
+        markTaskCompleted(taskId)
     }
 
     return (
         <TouchableOpacity
-            onPressIn={navigate}
+            onPressIn={complete}
             style={[styles.container, { bottom: insets.bottom, backgroundColor: green }]}
         >
             <ThemedText colorName='background' style={{ fontWeight: 'bold' }}>Complete</ThemedText>
--- components/TaskCard.tsx
+++ components/TaskCard.tsx
@@ -1,3 +1,4 @@
+import * as Haptics from 'expo-haptics';
 import { LinearGradient } from 'expo-linear-gradient';
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
 
@@ -7,11 +8,11 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 import { useTasks } from '@/providers/TaskProvider';
 import { Task } from '@/utils/types';
 import { router } from 'expo-router';
-import { Circle } from 'lucide-react-native';
+import { Circle, CircleCheck } from 'lucide-react-native';
 import React from 'react';
 
 export function TaskCard({ task, index }: { task: Task, index: number }) {
-    const { fetchTaskById, getMinMaxPriority } = useTasks()
+    const { markTaskCompleted, getMinMaxPriority } = useTasks()
     const { minPriority, maxPriority } = getMinMaxPriority()
     const borderColor = useThemeColor({ light: '#F2F0E5', dark: '#1C1B1A' }, 'background')
     const highlightColor = useThemeColor({ light: '#8B7EC8', dark: '#5E409D' }, 'tint')
@@ -54,35 +55,52 @@ export function TaskCard({ task, index }: { task: Task, index: number }) {
         // Determine which third the priority falls into
         if (task.priority < lowerThird) {
             return <View style={{ width: 8, height: 8, borderRadius: 8, backgroundColor: red }} />
-        } else if (task.priority < middleThird) {
-            return <View style={{ width: 8, height: 8, borderRadius: 8, backgroundColor: yellow }} />
+        // } else if (task.priority < middleThird) {
+        //     return <View style={{ width: 8, height: 8, borderRadius: 8, backgroundColor: yellow }} />
         } else {
             return <></>
         }
     }
 
+    function complete() {
+        if (process.env.EXPO_OS === 'ios') {
+            // Add a soft haptic feedback when pressing down on the tabs.
+            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid);
+        }
+        markTaskCompleted(task.id)
+    }
+
     return (
-        <TouchableOpacity
-            onPress={() => { router.navigate(`/${task.id}`) }}>
-            <LinearGradient
-                colors={[highlight, border]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: toDescendingRange(index), y: 0.8 }}
-                style={[styles.highlight, { backgroundColor: borderColor }]}
-            >
-                <ThemedView lightColor='#F2F0E5' darkColor='#1C1B1A' style={styles.cardOuter}>
-                    <View style={styles.taskBody}>
-                        <Circle color={iconColor} />
-                        {/* <CircleCheck /> */}
-                        <ThemedText type="defaultSemiBold" style={{ flex: 1 }}>{task.title}</ThemedText>
+
+        <LinearGradient
+            colors={[highlight, border]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: toDescendingRange(index), y: 0.8 }}
+            style={[styles.highlight, { backgroundColor: borderColor }]}
+        >
+            <ThemedView lightColor='#F2F0E5' darkColor='#1C1B1A' style={styles.cardOuter}>
+                <View style={styles.taskBody}>
+                    <TouchableOpacity
+                        onPress={() => complete()}
+                        style={{ paddingLeft: 16, paddingRight: 8, paddingVertical: 16 }}
+                    >
+                        {task.completed
+                            ? <CircleCheck color={iconColor} size={16} />
+                            : <Circle color={iconColor} size={16} />
+                        }
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { router.navigate(`/${task.id}`) }}
+                        style={{ flexDirection: 'row', flex: 1, alignItems: 'center', paddingVertical: 16, paddingRight: 16 }}
+                    >
+                        <ThemedText type="defaultSemiBold" style={[{ flex: 1 }, task.completed ? { color: iconColor, textDecorationLine: 'line-through' } : {}]}>{task.title}</ThemedText>
                         <View style={{ flexDirection: 'row', gap: 4 }}>
-                            {/* <View style={{ width: 8, height: 8, borderRadius: 8, backgroundColor: yellow }} /> */}
-                            <PriorityPip />
+                            {!task.completed && <PriorityPip />}
                         </View>
-                    </View>
-                </ThemedView>
-            </LinearGradient>
-        </TouchableOpacity>
+                    </TouchableOpacity>
+                </View>
+            </ThemedView>
+        </LinearGradient>
     );
 }
 
@@ -95,12 +113,9 @@ const styles = StyleSheet.create({
     taskBody: {
         display: 'flex',
         flexDirection: 'row',
-        gap: 8,
         alignItems: 'center'
     },
     cardOuter: {
-        paddingHorizontal: 16,
-        paddingVertical: 16,
         borderRadius: 6,
     },
     highlight: {
